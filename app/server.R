bbd2d2dce06aa83ec14040d8a6f12c652a62f6ad
msula2 <msula2@uis.edu>
2023-11-20 19:27:53 +0500

    Add About section to UI

--- app/server.R
+++ app/server.R
@@ -1,10 +1,58 @@
-server <- function(input, output) {
-  # Example plot outputs
-  output$plot1 <- renderPlot({
-    plot(cars)
+server <- function(input, output, session) {
+  v <- reactiveValues(
+    vars_plot = NULL,
+    vars_def = NULL
+  )
+  
+  observeEvent(input$navbar,{
+    tab <- input$navbar
+    if (tab == "Penman-Monteith") {
+      vars_ids <- list("net_radiation", "ground_heat_flux", "latent_heat", "psy_constant", "wind_speed", "vapor_sat", "vapor_act")
+      vars_text <- list("R_n", "G", "\\lambda", "\\gamma", "u_2", "e_s", "e_d")
+      v$vars_def <- list("net_radiation" = "Net Radiation", "ground_heat_flux"="Ground Heat Flux", "latent_heat"="Latent Heat of Vaporation", "psy_constant"="Psychometric Constant", "wind_speed"="Wind Speed 2m above ground", "vapor_sat"="Saturation Vapor Pressure", "vapor_act"= "Actual Vapor Pressure")
+      v$vars_plot <- setNames(vars_ids, vars_text)
+    }
+    
+    
+    updateSelectizeInput(
+      session, "plot_type",
+      choices = v$vars_plot,
+      options = list(
+        render = I("
+          {
+            item: function(item, escape) { 
+              var html = katex.renderToString(item.label);
+              return '<div>' + html + '</div>'; 
+            },
+            option: function(item, escape) { 
+              var html = katex.renderToString(item.label);
+              return '<div>' + html + '</div>'; 
+            }
+          }
+        ")
+      )
+    )
   })
   
-  output$plot2 <- renderPlot({
-    plot(pressure)
+  output$plot_by <- renderUI({
+    fluidRow(
+      column(6,
+             selectizeInput("plot_type", label = "Plot by:", choices = NULL)
+      ),
+      column(6,
+             uiOutput("definition"),
+             style = "display: flex; align-items: center; height: 85px;"
+      )
+    )
+    
+  })
+  
+  output$definition <- renderUI({
+    def <- v$vars_def[input$plot_type]
+    label_html <- sprintf('<i class="fa-regular fa-circle-question info" data-toggle="tooltip" data-placement="right" title="%s"></i>', def)
+    div(
+      HTML(label_html)
+    )
+    
   })
 }
--- app/ui.R
+++ app/ui.R
@@ -1,12 +1,17 @@
 library(shiny)
 library(shinythemes)
+library(katex)
 library(shinydashboard)
 
 # Define UI for application
 ui <- navbarPage(
   "Equations in Ecohydrology",
+  id = "navbar",
   tags$head(
-    tags$link(rel = "stylesheet", type = "text/css", href = "styles.css")
+    tags$link(rel = "stylesheet", href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"),
+    tags$link(rel = "stylesheet", type = "text/css", href = "styles.css"),
+    tags$link(rel="stylesheet", href="https://cdn.jsdelivr.net/npm/katex@0.10.0-beta/dist/katex.min.css", integrity="sha384-9tPv11A+glH/on/wEu99NVwDPwkMQESOocs/ZGXPoIiLE8MU/qkqUcZ3zzL+6DuH", crossorigin="anonymous"),
+    tags$script(src="https://cdn.jsdelivr.net/npm/katex@0.10.0-beta/dist/katex.min.js", integrity="sha384-U8Vrjwb8fuHMt6ewaCy8uqeUXv4oitYACKdB0VziCerzt011iQ/0TqlSlv8MReCm", crossorigin="anonymous")
   ),
   collapsible = TRUE,
   inverse = TRUE,
@@ -17,11 +22,31 @@ ui <- navbarPage(
     fluidPage(
       column(
         width = 8,
+        uiOutput("set_args")
+      ),
+      column(
+        width = 4,
         box(
-          width = NULL, title = "Graph", class = "custom-box", height = 500,
-          p("hello")
+          width = NULL, title = "About", class = "custom-box about-box",
+          p(
+            "The Penman-Monteith equation is a widely used method for calculating 
+              the evapotranspiration (ET) of water from the land surface into the atmosphere."
+          ),
+          p(
+            " Evapotranspiration is the combined process of evaporation, which is the loss 
+              of water from the land surface directly into the atmosphere, and transpiration, 
+              which is the release of water vapor from plants through their leaves."
+          ),
+          HTML(katex_html(
+            "\\lambda{ET} = \\frac{ \\Delta (R_n - G) + \\rho_a c_p \\frac{(e_s - e_a)}{r_a} }{\\Delta + \\gamma(1 + \\frac{r_s}{r_a})}",
+            displayMode = TRUE, 
+            preview = FALSE,
+            include_css = TRUE,
+            output = "html")
+          ),
+          uiOutput("plot_by")
+          
         )
-        
       )
     )
   )
