87fb552db7705aaf7eccc52e1571f91d0b865cc9
msula2 <msula2@uis.edu>
2024-01-21 18:07:41 +0500

    Restructure folders

--- app/app.R
+++ app/app.R
@@ -12,11 +12,11 @@ library(knitr)
 library(shinyjs)
 library(DT)
 library(shinyWidgets)
-source("ui_penman.R")
-source("ui_vapor.R")
-source("ui_open.R")
-source("ui_criddle.R")
-source("ui_pan.R")
+source("ui/ui_penman.R")
+source("ui/ui_vapor.R")
+source("ui/ui_open.R")
+source("ui/ui_criddle.R")
+source("ui/ui_pan.R")
 # Define UI for application
 ui <- navbarPage(
   "Evapotranspiration",
@@ -66,3 +66,38 @@ ui <- navbarPage(
     ui_pan
   )
 )
+
+# server.R
+
+# Source the server functions for each tab
+source("server/server_penman.R")
+source("server/server_open.R")
+source("server/server_vapor.R")
+source("server/server_criddle.R")
+source("server/server_pan.R")
+
+# Define the main server function
+server <- function(input, output, session) {
+  
+  observe({
+    # Get the currently selected tab
+    current_tab <- input$navbar
+    
+    # Call the corresponding server function based on the selected tab
+    if (current_tab == "Penman's Method") {
+      penman_server(input, output, session)
+    } else if (current_tab == "Meyer's Open Water Evaporation") {
+      open_server(input, output, session)
+    } else if (current_tab == "Vapor Pressure Deficit") {
+      vapor_server(input, output, session)
+    } else if (current_tab == "Blaney Criddle Method"){
+      criddle_server(input, output, session)
+    }
+    else if (current_tab == "Pan Evaporation"){
+      pan_server(input, output, session)
+    }
+  })
+}
+
+shinyApp(ui, server)
+
